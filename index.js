e3326e15369df31119ca0dc58de4873841d808db
emil <hoangominh01@gmail.com>
2023-07-06 14:37:51 +0700

    wip test

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const requestIp = require('request-ip');
-const {IP2Location} = require("ip2location-nodejs");
+const IpInfo = require('./src');
 
 const app = express();
 app.use(express.json());
-
-let ip2location = new IP2Location();
-
-ip2location.open("./IP2LOCATION-LITE.BIN");
+app.set('trust proxy', true);
 
 app.get('/', (request, response) => {
-    let ipAddress = requestIp.getClientIp(request);
+    let ipAddress = request.ip;
     response.send("Hello, " + ipAddress);
 });
 
-app.get('/ip2location', (request, response) => {
-    let ipAddress = request.query.ip;
-    if (!ipAddress) {
-        ipAddress = requestIp.getClientIp(request);
+app.get('/:driver', (request, response) => {
+    let ipAddress = request.query.ip ?? request.ip;
+    let driver = request.params.driver;
+    addition = null;
+    if (driver.toLocaleLowerCase() === 'maxmind') {
+        addition = './GeoLite2-Country.mmdb'
     }
-
-    let result = ip2location.getAll(ipAddress);
-
-    return response.json({
-        'ip': result.ip,
-        'country_name': result.countryLong,
-        'country_code': result.countryShort,
+    if(driver.toLocaleLowerCase() === 'ip2location') {
+        addition = './IP2LOCATION-LITE.BIN'
+    }
+    let infor = (new IpInfo(ipAddress, driver, addition)).get()
+    infor.then((data) => {
+        return response.json(data)
     })
-});
+    .catch((error) => {
+        return response.json(error)
+    });
+})
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
